5640fab686166d425e1d91be654e83c1fd7904ce
Anderson Santos <anderson.chenko1993@gmail.com>
2025-03-08 14:36:09 -0600

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Details from "./pages/Details";
+import Profile from "./pages/Profile";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="detais/:id" element={<Details />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
