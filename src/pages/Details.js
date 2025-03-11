3faedf5de953df821f061a249b94acc634363084
Anderson Santos <anderson.chenko1993@gmail.com>
2025-03-11 12:21:56 -0600

    Add header component

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,29 +1,31 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+import HeaderComponent from "../components/HeaderComponent";
+
 export default function Details() {
   const [journal, setJournal] = useState(null);
   let { id } = useParams();
 
   useEffect(() => {
-    console.log(window.innerHeight);
     const journals = JSON.parse(localStorage.getItem("journals")) || [];
     if (journals.length > 0) {
       const journal = journals.find((journal) => journal.id === id);
-      console.log("journal", journal);
       if (journal !== undefined) {
         setJournal(journal);
       }
     }
-  }, []);
+  }, [id]);
 
   return (
     <>
       {journal && (
-        <div className="grid text-center">
-          <h1 className="mt-4 mb-4">{journal.date}</h1>
-          <textarea value={journal.journal} rows="12" readOnly></textarea>
-        </div>
+        <>
+          <HeaderComponent page="/" header={journal.date} />
+          <div className="grid text-center">
+            <textarea value={journal.journal} rows="12" readOnly></textarea>
+          </div>
+        </>
       )}
     </>
   );
--- src/pages/NewJournal.js
+++ src/pages/NewJournal.js
@@ -1,33 +1,25 @@
 import { useContext } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
 
 import { AlertContext } from "../context/AlertContext";
-import FormComponent from "../components/FormComponent";
 import { formatDate } from "../utils/formatDate";
 
+import HeaderComponent from "../components/HeaderComponent";
+import FormComponent from "../components/FormComponent";
+
 import "../styles/new-journal.css";
-import icon from "../assets/icons/left.png";
 
 export default function NewJournal() {
   const { alert } = useContext(AlertContext);
-  const navigate = useNavigate();
 
   return (
     <>
-      <div className="d-flex flex-column text-center overflow-x-visible">
-        <div className="d-flex justify-content-center">
-          {/* <button className="mt-3 mb-3" onClick={() => navigate("/")}>
-            <img
-              src={icon}
-              alt="go to previous page"
-              className="align-self-center"
-            />
-          </button> */}
-          <h1 className="mt-4 mb-4 flex-shrink-1">{formatDate(new Date())}</h1>
-          <div></div>
-        </div>
-
-        {alert && <div className="d-inline p-2 alert text-white">{alert}</div>}
+      <HeaderComponent page="/" header={formatDate(new Date())} />
+      <div className="text-center overflow-x-visible">
+        {alert && (
+          <div className="d-inline align-self-center p-2 alert text-white">
+            {alert}
+          </div>
+        )}
         <FormComponent />
       </div>
     </>
