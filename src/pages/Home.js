ba50ff61f2a7498fa9cd4cee8e5d0e1f5aa813d5
Anderson Santos <anderson.chenko1993@gmail.com>
2025-03-12 18:06:09 -0600

    Add list journal component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import Sidebar from "../components/Sidebar";
 import ButtonComponent from "../components/ButtonComponent";
 
 import "../styles/home.css";
+import ListComponent from "../components/ListComponent";
 
 export default function Home() {
   const [journals, setJournals] = useState([]);
@@ -12,21 +13,8 @@ export default function Home() {
 
   useEffect(() => {
     const storedJournals = JSON.parse(localStorage.getItem("journals")) || [];
-
-    const groupedJournals = storedJournals.reduce((groups, journal) => {
-      (groups[journal.date] = groups[journal.date] || []).push(journal);
-      return groups;
-    }, {});
-
-    const journalsByDate = Object.entries(groupedJournals)
-      .map(([date, entries]) => ({ date, entries }))
-      .sort((a, b) => new Date(b.date) - new Date(a.date));
-
-    setJournals(journalsByDate);
+    setJournals(storedJournals);
   }, []);
-
-  const truncateText = (text, limit = 300) =>
-    text.length <= limit ? text : `${text.substring(0, limit)}...`;
 
   return (
     <div className="home-container">
@@ -42,27 +30,7 @@ export default function Home() {
 
       <main className="journal-list">
         {journals.length > 0 ? (
-          journals.map(({ date, entries }) => {
-            return (
-              <div key={date} className="date-group">
-                <div className="date-header">
-                  <span>{date}</span>
-                </div>
-
-                <div className="entries-container">
-                  {entries.map((entry) => (
-                    <Link
-                      to={`/journal/${entry.id}`}
-                      key={entry.id}
-                      className="journal-card"
-                    >
-                      <p>{truncateText(entry.journal)}</p>
-                    </Link>
-                  ))}
-                </div>
-              </div>
-            );
-          })
+          <ListComponent journals={journals} />
         ) : (
           <div className="no-entries">
             <p>No journal entries yet.</p>
